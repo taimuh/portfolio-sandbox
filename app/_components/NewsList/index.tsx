bab7fa2918385cb436d819b122b60fbf40318b34
timeh <taimutaitotaiga@gmail.com>
2025-04-20 19:30:44 +0900

    microcmsからコンテンツを取得するように変更しました。

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -18,15 +18,24 @@ export default function NewsList({ news }: Props) {
     <ul>
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
-          {/* TODO:別の章で、リンクと正式な画像に差し替えます */}
-          <Link href={"/news/${article.id}"} className={styles.link}>
-            <Image
-              className={styles.image}
-              src="/no-image.png"
-              alt="No Image"
-              width={1200}
-              height={630}
-            />
+          <Link href={`/news/${article.id}`} className={styles.link}>
+            {article.thumbnail ? (
+              <Image
+                src={article.thumbnail.url}
+                alt=""
+                className={styles.image}
+                width={article.thumbnail.width}
+                height={article.thumbnail.height}
+              />
+            ) : (
+              <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="No Image"
+                width={1200}
+                height={630}
+              />
+            )}
             <dl className={styles.content}>
               <dt className={styles.title}>{article.title}</dt>
               <dd className={styles.meta}>
